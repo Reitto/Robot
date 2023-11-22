556889a5466fff672276ddb07b800b75f6d3e4a1
adrian <adrianvisiedo2002@gmail.com>
2023-11-22 18:56:53 +0100

    ControladorRobot.java actualizado

---
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/ControladorRobot.java
@@ -0,0 +1,29 @@
+//Adrián Visiedo Rodríguez
+
+package org.iesalandalus.programacion.robot.modelo;
+
+import javax.naming.OperationNotSupportedException;
+import java.util.Objects;
+
+public class ControladorRobot {
+    Robot robot;
+
+    public ControladorRobot(Robot robot) {
+        Objects.requireNonNull(robot, "El robot no puede ser nulo.");
+        this.robot = new Robot();
+    }
+
+    public Robot getRobot() {
+        return new Robot();
+    }
+
+    public void ejecutar(char comando) throws OperationNotSupportedException {
+        switch (comando) {
+            case 'A', 'a' -> robot.avanzar();
+            case 'D', 'd' -> robot.girarALaDerecha();
+            case 'I', 'i' -> robot.girarALaIzquierda();
+            default -> throw new OperationNotSupportedException("Comando desconocido.");
+        }
+
+    }
+}
