7e994e8b7d286935a1b8d52a6af95ac22abf5019
adrian <adrianvisiedo2002@gmail.com>
2023-11-22 13:41:33 +0100

    Nombre, Orientación, Coordenada y zona actualizados

---
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Coordenada.java
@@ -0,0 +1,6 @@
+//Adrián Visiedo Rodríguez
+
+package org.iesalandalus.programacion.robot.modelo;
+
+public record Coordenada(int x, int y) {
+}
---
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Orientacion.java
@@ -0,0 +1,25 @@
+//Adrián Visiedo Rodríguez
+
+package org.iesalandalus.programacion.robot.modelo;
+
+public enum Orientacion {
+    NORTE ("Norte"),
+    SUR ("Sur"),
+    ESTE ("Este"),
+    OESTE ("Oeste"),
+    NORESTE ("Noreste"),
+    NOROESTE("Noroeste"),
+    SURESTE ("Sureste"),
+    SUROESTE ("Suroeste");
+
+    private final String cadenaMostrar;
+
+    Orientacion(String nombre) {
+        this.cadenaMostrar = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return cadenaMostrar;
+    }
+}
